8174e35f4f630b3e635a420871fa5a5fc2d1a757
MarcinPilarz <130612@student.uni.opole.pl>
2025-03-09 10:13:49 +0100

    Initialization project

---
+++ visitsdoctor/src/main/java/com/backend/visitsdoctor/VisitsdoctorApplication.java
@@ -0,0 +1,13 @@
+package com.backend.visitsdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitsdoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitsdoctorApplication.class, args);
+	}
+
+}
---
+++ visitsdoctor/src/test/java/com/backend/visitsdoctor/VisitsdoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.visitsdoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitsdoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
